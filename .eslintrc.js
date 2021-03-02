b8c1244b0be75f99fccfe1735438047d447794aa
MiyueSC <913784771@qq.com>
2021-03-02 11:15:59 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "prettier/prettier": [
+      "warn",
+      {
+        "useTabs": "false",
+        "singleQuote": false,
+        "printWidth": 100,
+        "semi": true,
+        "trailingComma": "none",
+        "bracketSpacing": true,
+        "jsxBracketSameLine": true,
+        "arrowParens": "avoid",
+        "requirePragma": false,
+        "proseWrap": "preserve"
+      }
+    ]
+  },
+};
