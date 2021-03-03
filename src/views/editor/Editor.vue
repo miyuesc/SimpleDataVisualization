30b3d5d1ff90030adc7e722e976f0df8ed2bde2e
MiyueSC <913784771@qq.com>
2021-03-03 17:33:22 +0800

    feat: 完善编辑页面结构

--- src/views/editor/Editor.vue
+++ src/views/editor/Editor.vue
@@ -1,14 +1,50 @@
 <template>
-  <div class="edit-container">
-    <drag-guide-lines />
+  <div class="editor-container">
+    <div class="editor-header"></div>
+    <div class="editor-slider"></div>
+    <div class="editor-context-pad"></div>
+    <div class="editor-content">
+      <!-- 背景元素区域 -->
+      <div class="editor-wrapper-area"></div>
+      <!-- 元素存在区域 -->
+      <div class="editor-canvas-area"></div>
+      <!-- 指示器区域 -->
+      <div class="editor-indicator-area">
+        <drag-guide-lines />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
+import { useStore } from "vuex";
+
+import DragGuideLines from "@/components/drag-cover/DragGuideLines.vue";
+
+export default {
+  name: "Editor",
+  components: {DragGuideLines},
+  setup() {
+    const store = useStore();
+    const dragState = store.state.dragModule;
+
+    return {
+      ...dragState,
+      updateLineVisible: () => {
+        console.log("dragState: ", dragState)
+        store.commit("dragModule/UPDATE_VISIBLE", !dragState.visible);
+      },
+      updateLinePosition: () => {
+        console.log("dragState: ", dragState)
+        store.commit("dragModule/UPDATE_POSITION", { left: Math.floor(Math.random() * 1000), top: Math.floor(Math.random() * 600)});
+      }
+    }
+  }
+}
 </script>
 
 <style scoped>
-.edit-container {
+.editor-container {
   position: relative;
   box-sizing: border-box;
   width: 100vw;
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,28 +8,8 @@
 </template>
 
 <script>
-import { useStore } from "vuex";
-
-import DragGuideLines from "@/components/drag-cover/DragGuideLines.vue";
 
 export default {
-  name: "Home",
-  components: {DragGuideLines},
-  setup() {
-    const store = useStore();
-    const dragState = store.state.dragModule;
-
-    return {
-      ...dragState,
-      updateLineVisible: () => {
-        console.log("dragState: ", dragState)
-        store.commit("dragModule/UPDATE_VISIBLE", !dragState.visible);
-      },
-      updateLinePosition: () => {
-        console.log("dragState: ", dragState)
-        store.commit("dragModule/UPDATE_POSITION", { left: Math.floor(Math.random() * 1000), top: Math.floor(Math.random() * 600)});
-      }
-    }
-  }
+  name: "Home"
 }
 </script>
